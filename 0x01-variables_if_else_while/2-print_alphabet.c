6caf00ee904646bc62d6cfa871a12a15bc2288b4
Maneli55 <sinoxolomaneli55@gmail.com>
2023-06-21 02:51:56 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,19 @@
+#include <stdio.h>
 
-for (ch = 'a' ; ch <= 'z' ; ch++)
+/**
+ *main - print alphabet in new line.
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
 		putchar(ch);
 	}
 
-	putchar(ch);
-	}
 	putchar('\n');
 	return (0);
 }
