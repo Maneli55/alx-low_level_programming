6f715d7f0178c731361e1c5eedf9e46ce34e27d8
Maneli55 <sinoxolomaneli55@gmail.com>
2023-06-21 02:59:49 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,15 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - print numbers from 0 to 9.
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
 	for (n = '0' ; n <= '9' ; n++)
 	{
 		putchar(n);
-		if (n ! = '9')
+		if (n != '9')
 		{
-
 			putchar(',');
-			putchar(',');
-
+			putchar(' ');
 		}
 	}
 
-		putchar('\n');
-		return(0);
+	putchar('\n');
+	return (0);
 }
